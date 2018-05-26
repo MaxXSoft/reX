078b6a9848d38acb351b1ca9cd11d3714fb55ba2
MaxXSoft <x@MaxXSoft.net>
2018-05-26 21:31:10 +0800

    (util) added header file 'util.h'

---
+++ src/re/util.h
@@ -0,0 +1,20 @@
+#ifndef REX_RE_UTIL_H_
+#define REX_RE_UTIL_H_
+
+#include <cstddef>
+
+namespace rex {
+
+inline void HashCombile(std::size_t &hash_value, std::size_t new_hash) {
+    const auto magic_num = 0xc6a4a7935bd1e995ULL;
+    const int offset = 47;
+    new_hash *= magic_num;
+    new_hash ^= new_hash >> offset;
+    new_hash *= magic_num;
+    hash_value ^= new_hash;
+    hash_value *= magic_num;
+}
+
+} // namespace rex
+
+#endif // REX_RE_UTIL_H_
